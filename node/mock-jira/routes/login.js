75697f65cf9e684511f052905cd8be5dcfabaab4
hualuo0609 <2406323182@qq.com>
2021-08-26 18:08:47 +0800

    node 注册接口 正在开发

--- node/mock-jira/routes/login.js
+++ node/mock-jira/routes/login.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
    res.send(req.query);
 });
-router.get('/username', function (req, res, next) {
+router.get('/register', function (req, res, next) {
    res.send(req.query);
 });
 
